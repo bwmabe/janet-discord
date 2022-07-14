6a6f93a24633c87bb8207aad909a0ce8783ec79d
bwmabe <9590854+bwmabe@users.noreply.github.com>
2022-07-13 20:10:08 -0400

    adding sleep helper

--- src/util.ts
+++ src/util.ts
@@ -4,6 +4,10 @@ function upperBoundedRand(upperBound: number): number {
     return Math.floor(Math.random() * upperBound)
 }
 
+export function sleep(duration: number) {
+    return new Promise<void>(r => setTimeout(r, duration * 1000))
+}
+
 export function ping(user: User): string {
     return `<@${user.id}>`
 }
@@ -14,10 +18,15 @@ export function getRand<Type>(array: Type[]): Type {
 
 export function containsAllowedDomain(url: string): boolean {
     const allowedDomains = ["i.reddit.com", "i.redd.it", "imgur.com", "gfycat.com"]
-    for(const domain in allowedDomains) {
-        if( url.includes(domain) ) {
-            return true
+    try {
+        for(const domain in allowedDomains) {
+            if( url.includes(domain) ) {
+                return true
+            }
         }
+    } catch (err){
+        console.error(err)
+        return false
     }
     return false
 }
